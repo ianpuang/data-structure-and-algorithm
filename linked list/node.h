1ae9027a51cf43af7f4bdbdcddfcb44a51c5cc6c
IAN NATHANIEL PUANG YI XUAN <ianpuang48@gmail.com>
2022-04-08 21:31:20 +0800

    a

---
+++ linked list/node.h
@@ -0,0 +1,40 @@
+#ifndef node_h
+#define node_h
+
+class Node
+{
+private:
+	int id;
+		
+
+public:
+	Node();
+	void setId(int newId);
+	int getId();
+	void setNextPtr(Node* newNextPtr);
+	Node* nextPtr; 
+	Node* getNextPtr();
+};
+
+Node::Node()
+{
+	id = -1 ;
+	nextPtr = nullPtr;
+	
+}
+
+void Node::setId(int newId)
+{
+	id = newId;
+}
+
+int Node::getId()
+{
+	return id;
+}
+
+Node* Node::setNextPtr(Node* newNextPtr)
+{
+	nextPtr = newNextPtr;
+}
+#endif
