2ac41f0f3696d2e85ae77e69ef38c35d71a859ef
IAN NATHANIEL PUANG YI XUAN <93971362+ianpuang@users.noreply.github.com>
2022-05-07 02:39:47 +0800

    add in listLength function

--- linked list/class node coding.cpp
+++ linked list/class node coding.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int main(
+struct node
+{
+	int data;
+	struct node* next;
+};
+
+int main()
 {
 	return 0;
+}
+
+int listLength(struct node* head)
+{
+	struct node *current = head;
+	int count = 0;
+	
+	while(current!=NULL)
+	{
+		count++;
+		current = current -> next;
+	}
+	return count;
 }
